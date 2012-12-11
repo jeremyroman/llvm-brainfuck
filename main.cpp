33bdb7dd22d00f857014132dd1581eb7c70e8e68
Jeremy Roman <jbroman@csclub.uwaterloo.ca>
2012-12-11 15:41:29 -0500

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <cassert>
 #include <cstdio>
+#include <stack>
 
 #include <llvm/LLVMContext.h>
 #include <llvm/Module.h>
@@ -11,17 +13,22 @@ static const uint64_t DATA_SIZE = 30000;
 static const char *DATA_NAME = "data";
 static const char *PTR_NAME = "ptr";
 
+struct Loop {
+	BasicBlock *Entry, *Body, *Exit;
+	PHINode *DataPtrBody, *DataPtrExit;
+};
+
 int main() {
   // LLVM context.
   LLVMContext &Context = getGlobalContext();
   Module MainModule("brainfuck program", Context);
   IRBuilder<> Builder(Context);
 
-  // Useful constants.
+  // Some useful constants.
   const IntegerType *CellType = Type::getInt8Ty(Context);
   Constant *One = ConstantInt::get(CellType, 1);
 
-  // Called by brainfuck code.
+  // Function prototypes for the shim.
   FunctionType *PutFunctionType = FunctionType::get(
       Type::getVoidTy(Context) /* return type */,
       std::vector<const Type *>(1, CellType) /* argument types */,
@@ -43,7 +50,7 @@ int main() {
       DATA_NAME);
   Value *DataPtr = Builder.CreateConstInBoundsGEP2_32(Data, 0, 0, PTR_NAME);
 
-  // Function signature: void brainfuck_main().
+  // Main function definition.
   FunctionType *MainFunctionType = FunctionType::get(
       Type::getVoidTy(Context) /* return type */,
       std::vector<const Type *>() /* argument types */,
@@ -56,6 +63,8 @@ int main() {
   // Code generation.
   int c;
   Value *Value;
+  std::stack<Loop> Loops;
+  Loop ThisLoop;
   while ((c = getchar()) != EOF) {
     switch (c) {
       case '>':
@@ -82,7 +91,63 @@ int main() {
         Value = Builder.CreateCall(GetFunction);
         Builder.CreateStore(Value, DataPtr);
         break;
+
+      case '[':
+        // Prepare data for the stack.
+        ThisLoop.Entry = Builder.GetInsertBlock();
+        ThisLoop.Body = BasicBlock::Create(Context, "loop", MainFunction);
+        ThisLoop.Exit = BasicBlock::Create(Context, "exit", MainFunction);
+
+        // Emit the beginning conditional branch.
+    	  Value = Builder.CreateLoad(DataPtr);
+    	  Value = Builder.CreateIsNotNull(Value);
+    	  Builder.CreateCondBr(Value, ThisLoop.Body, ThisLoop.Exit);
+
+    	  // Define the pointer after the loop.
+    	  Builder.SetInsertPoint(ThisLoop.Exit);
+    	  ThisLoop.DataPtrExit = Builder.CreatePHI(DataPtr->getType(), PTR_NAME);
+    	  ThisLoop.DataPtrExit->addIncoming(DataPtr, ThisLoop.Entry);
+
+    	  // Define the pointer within the loop.
+    	  Builder.SetInsertPoint(ThisLoop.Body);
+    	  ThisLoop.DataPtrBody = Builder.CreatePHI(DataPtr->getType(), PTR_NAME);
+    	  ThisLoop.DataPtrBody->addIncoming(DataPtr, ThisLoop.Entry);
+
+    	  // Continue generating code within the loop.
+    	  DataPtr = ThisLoop.DataPtrBody;
+    	  Loops.push(ThisLoop);
+    	  break;
+
+    	case ']':
+        // Pop data off the stack.
+    	  if (Loops.empty()) {
+        	fputs("] requires matching [\n", stderr);
+        	abort();
+        }
+    	  ThisLoop = Loops.top();
+    	  Loops.pop();
+
+    	  // Finish off the phi nodes.
+    	  ThisLoop.DataPtrBody->addIncoming(DataPtr, Builder.GetInsertBlock());
+    	  ThisLoop.DataPtrExit->addIncoming(DataPtr, Builder.GetInsertBlock());
+
+    	  // Emit the ending conditional branch.
+    	  Value = Builder.CreateLoad(DataPtr);
+    	  Value = Builder.CreateIsNotNull(Value);
+    	  Builder.CreateCondBr(Value, ThisLoop.Body, ThisLoop.Exit);
+
+    	  // Move insertion after the loop.
+    	  ThisLoop.Exit->moveAfter(Builder.GetInsertBlock());
+    	  DataPtr = ThisLoop.DataPtrExit;
+    	  Builder.SetInsertPoint(ThisLoop.Exit);
+    	  break;
     }
+  }
+
+  // Ensure all loops have been closed.
+  if (!Loops.empty()) {
+  	fputs("[ requires matching ]\n", stderr);
+  	abort();
   }
 
   // Finish off brainfuck_main and dump.
