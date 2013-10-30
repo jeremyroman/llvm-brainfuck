79690ca2a6dd9a18dc476a5fc2ab7349daee1243
Muhammad Tauqir Ahmad <mtahmed@uwaterloo.ca>
2013-10-30 18:48:16 -0400

    Alphabetize the llvm headers.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include <cstdio>
 #include <stack>
 
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
-#include <llvm/IR/IRBuilder.h>
 #include <llvm/Support/raw_ostream.h>
 
 using namespace llvm;
